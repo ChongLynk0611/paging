8c3f946aa48e75c5e43405bcfd8d08004f570b0b
ChongLynk <tronglinhmta0611@gmail.com>
2020-08-05 08:34:37 -0400

    Hoan thanh phan paging

--- controller/index.controller.js
+++ controller/index.controller.js
@@ -1,8 +1,13 @@
 var db = require('../db');
 
 module.exports.index = function(req, res ){
-    
+    var page = parseInt( req.query.page) || 1;
+    var start = (page-1)*6;
+    var end = page*6;
+    var pages = [String( page-1),String(page),String( page+1),String( page+2)];
     res.render('index',{
-        products:db.get('products').value()
+        products:db.get('products').value().slice(start,end),
+        pages:pages
+        
     });
 }
